645065127ca640f3dcd80beca15888e2f9c194c3
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-09-09 21:18:15 +0530

    Round 1049 div2

---
+++ ShiftSort.java
@@ -0,0 +1,103 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.io.BufferedReader;
+
+import java.util.InputMismatchException;
+
+public class ShiftSort {
+    public static void main(String args[])throws IOException {
+        BfReader obj = new BfReader(System.in);
+        FastPrinter out = new FastPrinter(System.out);
+        int tests = obj.readInt();
+        obj.readLine();
+        for(int i = 0; i < tests; i++){
+            int n = Integer.parseInt(obj.readLine());
+            String[] str = obj.readLine().split("");
+            int zero = 0;
+            for(int j = 0; j < n; j++){
+                if(str[j].equals("0")) zero++;
+            }
+            int k = 0;
+            for(int j = 0; j < zero; j++){
+                if(str[j].equals("1")){
+                    k++;
+                }
+            }
+            out.println(k);
+        }
+        out.close();
+    }
+
+    //FastScanner and FastWriter implementation from SecondThread (codeforces)
+    static class BfReader extends BufferedReader{
+        public BfReader(InputStream in){
+            super(new InputStreamReader(in));
+        }
+        static boolean isWhiteSpace(int c) {
+            return c >= 0 && c <= 32;
+        }
+        public String next() {
+            StringBuilder sb = new StringBuilder();
+            int c = read();
+            while (isWhiteSpace(c)) {
+                c = read();
+            }
+            if (c < 0) {
+                return null;
+            }
+            while (c >= 0 && !isWhiteSpace(c)) {
+                sb.appendCodePoint(c);
+                c = read();
+            }
+            return sb.toString();
+        }
+        public int read() {
+            try {
+                int ret = super.read();
+                return ret;
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+        }
+        public String readLine() {
+            try {
+                return super.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        public int readInt() {
+            int c = read();
+            while (isWhiteSpace(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int ret = 0;
+            while (c >= 0 && !isWhiteSpace(c)) {
+                if (c < '0' || c > '9') {
+                    throw new NumberFormatException("digit expected " + (char) c
+                            + " found");
+                }
+                ret = ret * 10 + c - '0';
+                c = read();
+            }
+            return ret * sgn;
+        }
+    }
+    static class FastPrinter extends PrintWriter {
+        public FastPrinter(OutputStream out) {
+            super(out);
+        }
+        public FastPrinter(Writer out) {
+            super(out);
+        }
+    }
+}
